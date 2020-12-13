e8406ac09d90a01a81f450526636af7b0ef3ad81
Mohamed Charaabi <41237052+medch373@users.noreply.github.com>
2020-12-13 14:59:49 +0100

    working on home page

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import youtube_dl
-import pafy
+from flask_material import Material
+
 
 app = Flask(__name__)
 
@@ -20,7 +21,7 @@ ydl_opts = {
 @app.route('/<string:url>', methods=['GET'])
 def form(url):
     # if request.method == "POST":
-    mp4format = []
+    videoformat = []
     audioFormat = []
     title = ""
     videoUrl = "www.youtube.com/watch?v=" + url
@@ -35,19 +36,63 @@ def form(url):
     #      print('the url =>', urls[0])
     #      c = urls[0]
         mp4 = [x for x in urls if x["ext"] == "mp4"]
-        mp4Quality = ["394", "395", "396", "397", "398", "399"]
+        mp4Quality = ["395", "396", "397", "398", "399", "134", "137", "22", "18"]
+        mp3Quality = ["394", "140", "139"]
 
         for z in urls:
             a = z["format_id"]
-            if a == "140":
+            if a in mp3Quality:
                 audioFormat.append(z)
 
             if a in mp4Quality:
-                mp4format.append(z)
+                videoformat.append(z)
 
-    return jsonify({"data": {"title": title, "mp4": mp4format, "audio": audioFormat}})
+    return jsonify({"data": {"title": title, "video": videoformat, "audio": audioFormat}})
 
+
+
+@app.route('/', methods=['GET', 'POST'])
+def facebook():
+    if request.method == "POST":
+        videoformat = []
+        audioFormat = []
+        title = ""
+        url = request.form.get("videoUrl")
+        with youtube_dl.YoutubeDL() as ydl:
+            ydl.cache.remove()
+            r = ydl.extract_info(url=url, download=False)
+        # title
+            title = r["title"]
+
+            urls = r["formats"]
+    #      print('the url =>', urls[0])
+    #      c = urls[0]
+            mp4 = [x for x in urls if x["ext"] == "mp4"]
+            mp4Quality = ["394", "395", "396", "397", "398",
+                          "399", "134", "137", "22", "18"]
+            mp3Quality = ["140", "139"]
+            formatNote = ["144p", "360p", "480p", "720p", "1080p"]
+
+            for z in urls:
+                a = z["format_id"]
+                if a in mp3Quality:
+                    audioFormat.append(z)
+
+                if a in mp4Quality:
+                    videoformat.append(z)
+
+        return render_template('result.html', audio=audioFormat, video=videoformat)
+
+    return render_template('form.html')
+
+
+
+
+
+@app.route('/')
+def index():
+   return render_template("index.html")
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    app.run()
