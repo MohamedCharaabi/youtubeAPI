0bb0f04a9a774a8f4bc90ad4eb44d8c1a7762682
Mohamed Charaabi <41237052+medch373@users.noreply.github.com>
2020-12-13 21:56:09 +0100

    hope this fix

--- index.py
+++ index.py
@@ -89,10 +89,10 @@ def facebook():
 
 
 
-@app.route('/')
-def index():
-   return render_template("index.html")
+# @app.route('/')
+# def index():
+#    return render_template("index.html")
 
 if __name__ == '__main__':
-
+    app.debug = True
     app.run()
